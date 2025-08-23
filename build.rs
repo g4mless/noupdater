e416d301471c14910268e8791a7258f143a6124c
g4mless <g4mless@proton.me>
2025-08-23 14:35:45 +0700

    add icon

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_manifest_file("app.manifest");
+        res.set_icon("icon.ico");
         res.compile().expect("Failed to compile resources");
     }
 }
